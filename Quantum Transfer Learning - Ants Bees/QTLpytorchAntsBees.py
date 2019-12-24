ae2c371bfd2794e04090fa46729b131c540a56af
Pasam Revanth kumar <33693651+pasamrevanthkumar1998@users.noreply.github.com>
2019-12-24 13:21:59 +0530

    Add files via upload

---
+++ Quantum Transfer Learning - Ants Bees/QTLpytorchAntsBees.py
@@ -0,0 +1,290 @@
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, models, transforms
+
+
+import pennylane as qml
+from pennylane import numpy as np
+
+import time
+import os
+import copy
+
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
+n_qubits = 4                
+step = 0.0004               
+batch_size = 4              
+num_epochs = 1              
+q_depth = 6                 
+gamma_lr_scheduler = 0.1    
+q_delta = 0.01              
+rng_seed = 1                
+start_time = time.time()
+
+dev = qml.device("default.qubit", wires=n_qubits)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+data_transforms = {
+    "train": transforms.Compose(
+        [
+            
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    ),
+}
+
+data_dir = "/content/drive/My Drive/datasets/hymenoptera_data"
+image_datasets = {
+    x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ["train", "val"]
+}
+dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
+class_names = image_datasets["train"].classes
+
+
+dataloaders = {
+    x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True)
+    for x in ["train", "val"]
+}
+
+
+def imshow(inp, title=None):
+    """Display image from tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+        
+inputs, classes = next(iter(dataloaders["val"]))
+
+
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
+
+
+
+torch.manual_seed(rng_seed)
+dataloaders = {
+    x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True)
+    for x in ["train", "val"]
+}
+
+def H_layer(nqubits):
+    """Layer of single-qubit Hadamard gates.
+    """
+    for idx in range(nqubits):
+        qml.Hadamard(wires=idx)
+
+
+def RY_layer(w):
+    """Layer of parametrized qubit rotations around the y axis.
+    """
+    for idx, element in enumerate(w):
+        qml.RY(element, wires=idx)
+
+
+def entangling_layer(nqubits):
+    """Layer of CNOTs followed by another shifted layer of CNOT.
+    """
+    
+    for i in range(0, nqubits - 1, 2):  
+        qml.CNOT(wires=[i, i + 1])
+    for i in range(1, nqubits - 1, 2):  
+        qml.CNOT(wires=[i, i + 1])
+        
+@qml.qnode(dev, interface="torch")
+def q_net(q_in, q_weights_flat):
+
+    
+    q_weights = q_weights_flat.reshape(q_depth, n_qubits)
+
+    
+    H_layer(n_qubits)
+
+    
+    RY_layer(q_in)
+
+    
+    for k in range(q_depth):
+        entangling_layer(n_qubits)
+        RY_layer(q_weights[k])
+
+    
+    exp_vals = [qml.expval(qml.PauliZ(position)) for position in range(n_qubits)]
+    return tuple(exp_vals)
+
+class Quantumnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.pre_net = nn.Linear(512, n_qubits)
+        self.q_params = nn.Parameter(q_delta * torch.randn(q_depth * n_qubits))
+        self.post_net = nn.Linear(n_qubits, 2)
+
+    def forward(self, input_features):
+        pre_out = self.pre_net(input_features)
+        q_in = torch.tanh(pre_out) * np.pi / 2.0
+
+        
+        q_out = torch.Tensor(0, n_qubits)
+        q_out = q_out.to(device)
+        for elem in q_in:
+            q_out_elem = q_net(elem, self.q_params).float().unsqueeze(0)
+            q_out = torch.cat((q_out, q_out_elem))
+        return self.post_net(q_out)
+    
+model_hybrid = torchvision.models.resnet18(pretrained=True)
+
+for param in model_hybrid.parameters():
+    param.requires_grad = False
+
+
+
+model_hybrid.fc = Quantumnet()
+
+
+model_hybrid = model_hybrid.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer_hybrid = optim.Adam(model_hybrid.fc.parameters(), lr=step)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_hybrid, step_size=10, gamma=gamma_lr_scheduler)
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
+    since = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    best_loss = 10000.0  
+    best_acc_train = 0.0
+    best_loss_train = 10000.0  
+    print("Training started:")
+
+    for epoch in range(num_epochs):
+
+        
+        for phase in ["train", "val"]:
+            if phase == "train":
+                scheduler.step()
+                
+                model.train()
+            else:
+                
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+
+            
+            n_batches = dataset_sizes[phase] // batch_size
+            it = 0
+            for inputs, labels in dataloaders[phase]:
+                since_batch = time.time()
+                batch_size_ = len(inputs)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
+
+                
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                
+                running_loss += loss.item() * batch_size_
+                batch_corrects = torch.sum(preds == labels.data).item()
+                running_corrects += batch_corrects
+                print(
+                    "Phase: {} Epoch: {}/{} Iter: {}/{} Batch time: {:.4f}".format(
+                        phase,
+                        epoch + 1,
+                        num_epochs,
+                        it + 1,
+                        n_batches + 1,
+                        time.time() - since_batch,
+                    ),
+                    end="\r",
+                    flush=True,
+                )
+                it += 1
+
+            
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+            print(
+                "Phase: {} Epoch: {}/{} Loss: {:.4f} Acc: {:.4f}        ".format(
+                    "train" if phase == "train" else "val  ",
+                    epoch + 1,
+                    num_epochs,
+                    epoch_loss,
+                    epoch_acc,
+                )
+            )
+
+            
+            if phase == "val" and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == "val" and epoch_loss < best_loss:
+                best_loss = epoch_loss
+            if phase == "train" and epoch_acc > best_acc_train:
+                best_acc_train = epoch_acc
+            if phase == "train" and epoch_loss < best_loss_train:
+                best_loss_train = epoch_loss
+
+    
+    model.load_state_dict(best_model_wts)
+    time_elapsed = time.time() - since
+    print("Training completed in {:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60))
+    print("Best test loss: {:.4f} | Best test accuracy: {:.4f}".format(best_loss, best_acc))
+    return model
+
+model_hybrid = train_model(
+    model_hybrid, criterion, optimizer_hybrid, exp_lr_scheduler, num_epochs=num_epochs
+)
+
+def visualize_model(model, num_images=6, fig_name="Predictions"):
+    images_so_far = 0
+    _fig = plt.figure(fig_name)
+    model.eval()
+    with torch.no_grad():
+        for _i, (inputs, labels) in enumerate(dataloaders["val"]):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images // 2, 2, images_so_far)
+                ax.axis("off")
+                ax.set_title("[{}]".format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+                if images_so_far == num_images:
+                    return
+                
+visualize_model(model_hybrid, num_images=batch_size)
+plt.show()
